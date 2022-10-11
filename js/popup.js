70589b1f432f1965b50f2a2b296a26b88227e644
nfabri <fabri.nicolas@yahoo.fr>
2022-10-11 16:53:05 +0200

    add popup system

---
+++ js/popup.js
@@ -0,0 +1,24 @@
+window.onload = onLoad
+
+async function onLoad () {
+ document.getElementById("donation_button").addEventListener("click", donate);
+ const result = await chrome.storage.sync.get(['nbWordsReplaced'])
+
+ let nbWordsReplaced
+ if (result.nbWordsReplaced) {
+  nbWordsReplaced = result.nbWordsReplaced
+ } else {
+  nbWordsReplaced = 0
+ }
+
+ const price = Math.round(nbWordsReplaced * 0.10 * 100) / 100;
+
+ document.getElementById('number').innerText = nbWordsReplaced
+ document.getElementById('donation_price').value = price
+}
+
+function donate() {
+ const price = document.getElementById('donation_price').value;
+
+ chrome.tabs.create({url: "https://soutenir.amnesty.fr/b?frequency=once&amount=" + (price * 100)});
+}
--- js/script.js
+++ js/script.js
@@ -16,11 +16,56 @@ String.prototype.replaceArray = function (find, replace) {
  return replaceString
 }
 
-function replacer () {
- let bannedWords = ['Qatar', 'Coupe du monde', 'Lusail', 'Al Khor', 'Doha', 'Iconic Stadium', 'Al Bayt Stadium', 'Stadium 974', 'Al Thumama Stadium', 'Al Rayyan', 'Khalifa International Stadium', 'Education City Stadium', 'Ahmad bin Ali Stadium', 'Al Wakrah', 'Al Janoub Stadium', 'Al Rihla', 'Hayya Hayya']
- let replaceWords = ['Q•••r', 'C•••e du monde', 'Lu••l', 'Al K••r', 'D•ha', 'I••••• Stadium', 'Al B••• Stadium', 'Stadium +4000', 'Al T••••• Stadium', 'Al R••••', 'K•••• In••••• Stadium', 'E•••• C•• Stadium', 'A••• •• A•• Stadium', 'Al W••••', 'Al J•••• Stadium', 'Al R••••', 'H••• H•••']
+async function replacer () {
+ const result = await chrome.storage.sync.get(['nbWordsReplaced'])
+
+ let bannedWords = [
+  'Qatar',
+  'Coupe du monde',
+  'Lusail',
+  'Al Khor',
+  'Doha',
+  'Iconic Stadium',
+  'Al Bayt Stadium',
+  'Stadium 974',
+  'Al Thumama Stadium',
+  'Al Rayyan',
+  'Khalifa International Stadium',
+  'Education City Stadium',
+  'Ahmad bin Ali Stadium',
+  'Al Wakrah',
+  'Al Janoub Stadium',
+  'Al Rihla',
+  'Hayya Hayya']
+ let replaceWords = [
+  'Q•••r',
+  'C•••e du monde',
+  'Lu••l',
+  'Al K••r',
+  'D•ha',
+  'I••••• Stadium',
+  'Al B••• Stadium',
+  'Stadium +4000',
+  'Al T••••• Stadium',
+  'Al R••••',
+  'K•••• In••••• Stadium',
+  'E•••• C•• Stadium',
+  'A••• •• A•• Stadium',
+  'Al W••••',
+  'Al J•••• Stadium',
+  'Al R••••',
+  'H••• H•••']
 
  const elements = document.getElementsByTagName('*')
+
+ let nbWordsReplacedTotal
+ let nbWordsReplaced = 0
+
+ if (result.nbWordsReplaced) {
+  nbWordsReplacedTotal = result.nbWordsReplaced
+ } else {
+  nbWordsReplacedTotal = 0
+ }
 
  for (let i = 0; i < elements.length; i++) {
   const element = elements[i]
@@ -34,9 +79,32 @@ function replacer () {
     const replacedText = text.replaceArray(bannedWords, replaceWords)
 
     if (replacedText !== text) {
+     nbWordsReplaced++
      element.replaceChild(document.createTextNode(replacedText), node)
     }
    }
   }
  }
+
+ if (nbWordsReplaced > 0) {
+  console.log("on passe la")
+  let post = JSON.stringify({
+   nbWordsReplaced: nbWordsReplaced
+  })
+
+  fetch("https://us-central1-wichone-94ba5.cloudfunctions.net/qatarBlockerCountFunction", {
+   method: 'post',
+   body: post,
+   headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+   }
+  }).then()
+ }
+
+ nbWordsReplaced += nbWordsReplacedTotal
+
+ chrome.storage.sync.set({
+  nbWordsReplaced: nbWordsReplaced,
+ })
 }
